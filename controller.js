28e6e57d431985f3456fd1c98a53a7a7d7a178ed
iris carolina <batista.iris@alunoescolassociais.org.br>
2023-09-29 11:46:00 -0300

    funcao par preencher o formulario com os dados de endereco com via cep

--- controller.js
+++ controller.js
@@ -14,3 +14,11 @@ const limparFormulario = () => {
 // verifica se CEP é valido
 const eNumero = (numero) => /^~[0-9]+$/.test(numero); //testa numero informado com expressão regular
 const cepValido = (cep) => cep.length == 8 && eNumero(cep); //verifica o tamanho do cep digitado e executa função de validação do cep eNumero
+
+//função para preencher formulário
+const preencherFormulario = (endereco) =>{
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value =endereco.uf;
+}
